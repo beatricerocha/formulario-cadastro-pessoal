deaa6ecc63382ca8daea1b5f1c64492374d3b940
beatricerocha <beatricerocha.it@gmail.com>
2021-10-23 23:39:19 -0300

    teste bd php

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const app = express();
+
+app.get('/cadastro', function(req, res){
+    res.render('cadastro');
+});
+
+app.listen('8080');
+
+
+var db = openDatabase ("pacien", "2.0", "Form", 4048);
+db.transaction(function (criar){
+    criar.executeSql("CREATE TABLE pacien (id PRIMARY KEY, nome TEXT, email TEXT, cpf INTEIRO, rg INTEIRO)");
+}); 
+
+function salvar (){
+    var nome = document.getElementById('nome').value;
+    var email = document.getElementById('email').value;
+    var cpf = document.getElementById('cpf').value;
+    var RG = document.getElementById('rg').value;
+    db.transaction(function (armazenar){
+        armazenar.executeSql("INSERT INTO pacien (nome, email, cpf,rg) VALUES (?, ?, ?, ?)", [nome, email, cpf, rg]);
+    });
+}
